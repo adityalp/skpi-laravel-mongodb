e1344ae1944e29597d9c574ae02cd3c415eec374
Dadan Abdullah <hallo.dadan@gmail.com>
2021-01-17 16:37:00 +0700

    initial commit

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Buku;
+
+class BukuController extends Controller
+{
+	public function index()
+	{
+		$buku = Buku::all();
+		return view('home', compact('buku'));
+	}
+}
